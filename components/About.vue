56c6b90a0374898472fa03a295a147330c323775
YuriiKovalenko <yurii.kovalenko@databand.ai>
2023-09-02 13:13:19 +0200

    fix: fix about page

--- components/About.vue
+++ components/About.vue
@@ -3,7 +3,6 @@
     <h1>About</h1>
     <p>Hey there! My name is Yurii Kovalenko. I am a frontend developer since 2010 from Melitopol, Ukraine.</p>
     <h3>Background</h3>
-    <v-divider class="pb-6" />
     <p>First steps in development I made in school. From that time it is my hobby and full-time  job.
 
       I started my career as a full stack developer on PHP and it finished by creating in-house CMS on YIi.</p>
@@ -14,7 +13,6 @@
     <p>Big fan of learning and curious to new experience and knowledge.</p>
     <p>I have a lot of hobbies not related to tech like snowboarding, sailing, music, traweling, cooking and more others.</p>
   <h3>Keep in touch</h3>
-    <v-divider class="pb-6" />
     <ul>
       <li><a target="_blank" href="https://www.linkedin.com/in/yurii-kovalenko-00718a51/">LinkedIn</a></li>
       <li><a target="_blank" href="https://twitter.com/CodexMen">Twitter</a></li>
