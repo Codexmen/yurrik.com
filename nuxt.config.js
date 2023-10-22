8814472dd3220fa641752121a48eb9b81ee70483
YuriiKovalenko <yurii.kovalenko@databand.ai>
2023-10-22 15:32:35 +0200

    fix: restore Gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,19 @@
 export default defineNuxtConfig({
     app: {
-        // htmlAttrs: {
-        //   lang: 'en'
-        // },
         head: {
             title: 'kovalenko-blog',
             titleTemplate: '%s | Kovalenko Yurii',
         },
-
-        // { charset: 'utf-8' },
-        // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        // { hid: 'description', name: 'description', content: '' },
-        // { name: 'format-detection', content: 'telephone=no' }
-        // link: [
-        //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        // ]
     },
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    // plugins: [
-    // ],
-    //
-    // // Auto import components: https://go.nuxtjs.dev/config-components
-    // components: true,
     css: ['~/assets/css/main.css'],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
     modules: [
         '@nuxt/content',
-        // '@nuxtjs/gtm',
+        'nuxt-gtag',
         '@invictus.codes/nuxt-vuetify',
     ],
-
-    // publicRuntimeConfig: {
-    //   gtm: {
-    //     id: process.env.GOOGLE_TAG_MANAGER_ID
-    //   }
-    // },
+    gtag: {
+        id: process.env.GOOGLE_TAG_MANAGER_ID
+    },
     vuetify: {
         vuetifyOptions: {
             theme: {
@@ -62,25 +39,7 @@ export default defineNuxtConfig({
             anchorLinks: false,
         },
         highlight: {
-            // Theme used in all color schemes.
             theme: 'github-dark',
         }
-    },
-//     moduleOptions: {
-//         treeshaking: true,
-//     }
-// }
-
-// // Build Configuration: https://go.nuxtjs.dev/config-build
-// build: {
-// },
-// target: 'static',
-// content: {
-//   markdown: {
-//     prism: {
-//       theme: 'prism-themes/themes/prism-cb.css'
-//     }
-//   }
-// }
-})
-;
+    }
+});
