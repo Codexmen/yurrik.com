c5fb1fc178d22e7baee24fa849d7a7aa440a2842
YuriiKovalenko <yurii.kovalenko@databand.ai>
2023-11-06 23:29:44 +0100

    feat: add photo to HeroSection.vue

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,26 +1,27 @@
 <template>
-  <v-container>
-    <v-row class="pt-8" align="center">
-      <v-col cols="12" md="6">
-          <div class="text-h3 pb-3">Hello, there! My name is Yurii Kovalenko</div>
-          <p class="subheading">I am a FrontEnd Developer from Ukraine</p>
-          <NuxtLink to="/about">
-            <v-btn color="primary" large class="mr-4">About me</v-btn>
-          </NuxtLink>
-          <NuxtLink to="/blog">
-            <v-btn color="secondary" large>Posts</v-btn>
-          </NuxtLink>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-img
-            width="600"
-            height="400"
-            alt="Yurii Kovalenko photo"
-            src="https://placehold.co/600x400"
-            max-height="400"
-            contain
-        ></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row class="pt-8" align="center">
+    <v-col cols="12" md="7">
+      <div class="text-h3 pb-3">Welcome there! My name is Yurii Kovalenko.</div>
+      <p class="subheading">I am a Front End Developer from Ukraine 🇺🇦</p>
+      <p class="subheading">You could learn more about me or check out my the latest posts.</p>
+      <NuxtLink to="/about">
+        <v-btn color="primary" large class="mr-4">About me</v-btn>
+      </NuxtLink>
+      <NuxtLink to="/blog">
+        <v-btn color="secondary" large>Posts</v-btn>
+      </NuxtLink>
+    </v-col>
+    <v-col cols="12" md="5" class="d-flex flex-row-reverse">
+      <v-img
+          lazy-src="/photo_small.jpeg"
+          max-width="400"
+          max-height="400"
+          width="400"
+          height="400"
+          alt="Yurii Kovalenko photo"
+          src="/photo.jpeg"
+          contain
+      ></v-img>
+    </v-col>
+  </v-row>
 </template>
